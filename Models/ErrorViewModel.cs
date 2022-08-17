e8caf17d33e4e68861809083e4d0f3570cdc01b3
Renan Martins <renanmartins1999@hotmail.com>
2022-08-17 09:09:18 -0300

    Initializing repo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _21_days_of_code.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
