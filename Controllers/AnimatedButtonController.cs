c057c2a162163838c4f736a9729692239da89dea
Renan Martins <renanmartins1999@hotmail.com>
2022-08-18 14:44:25 -0300

    Finished Day 3

---
+++ Controllers/AnimatedButtonController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace _21_days_of_code.Controllers;
+
+public class  AnimatedButtonController : Controller
+{
+    public IActionResult Index(){
+        return View();
+    }
+}
