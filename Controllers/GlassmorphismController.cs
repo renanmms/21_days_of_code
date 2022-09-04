5d9b8242981159f99968ca89c98c27d7803d0e2d
Renan Martins <renanmartins1999@hotmail.com>
2022-09-03 21:04:55 -0300

    Initializing Day 6

---
+++ Controllers/GlassmorphismController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace _21_days_of_code.Controllers;
+
+public class GlassmorphismController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
